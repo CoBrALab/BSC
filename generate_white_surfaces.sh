6b12ae16c21bc1e646eb499123962ed2c27ebca4
parent41 <oparent456@gmail.com>
2020-11-16 23:04:18 -0500

    minor change

--- generate_white_surfaces.sh
+++ generate_white_surfaces.sh
@@ -10,7 +10,7 @@ do
 ./methods/move_surface_along_flipped_vector $file ./surfaces/GM_25_surfaces/$(basename $file _WM_0_surface_left.obj)_GM_25_surface_left.obj ./surfaces/WM_25_surfaces/$(basename $file _WM_0_surface_left.obj)_WM_25_surface_left.obj
 ./methods/move_surface_along_flipped_vector $file ./surfaces/GM_18_75_surfaces/$(basename $file _WM_0_surface_left.obj)_GM_18_75_surface_left.obj ./surfaces/WM_18_75_surfaces/$(basename $file _WM_0_surface_left.obj)_WM_18_75_surface_left.obj
 ./methods/move_surface_along_flipped_vector $file ./surfaces/GM_6_25_surfaces/$(basename $file _WM_0_surface_left.obj)_GM_6_25_surface_left.obj ./surfaces/WM_6_25_surfaces/$(basename $file _WM_0_surface_left.obj)_WM_6_25_surface_left.obj
-echo $(basename $file _WM_0_surface_left.obj)
+echo $(basename $file _WM_0_surface_left.obj) left
 done
 
 for file in ./surfaces/WM_0_surfaces/*right*;
@@ -19,5 +19,5 @@ do
 ./methods/move_surface_along_flipped_vector $file ./surfaces/GM_25_surfaces/$(basename $file _WM_0_surface_right.obj)_GM_25_surface_right.obj ./surfaces/WM_25_surfaces/$(basename $file _WM_0_surface_right.obj)_WM_25_surface_right.obj
 ./methods/move_surface_along_flipped_vector $file ./surfaces/GM_18_75_surfaces/$(basename $file _WM_0_surface_right.obj)_GM_18_75_surface_right.obj ./surfaces/WM_18_75_surfaces/$(basename $file _WM_0_surface_right.obj)_WM_18_75_surface_right.obj
 ./methods/move_surface_along_flipped_vector $file ./surfaces/GM_6_25_surfaces/$(basename $file _WM_0_surface_right.obj)_GM_6_25_surface_right.obj ./surfaces/WM_6_25_surfaces/$(basename $file _WM_0_surface_right.obj)_WM_6_25_surface_right.obj
-echo $(basename $file _WM_0_surface_right.obj)
+echo $(basename $file _WM_0_surface_right.obj) right
 done
