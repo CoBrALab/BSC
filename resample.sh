f9737be0679078d56dcb0ea23872fa181ce24136
parent41 <oparent456@gmail.com>
2021-02-28 13:45:43 -0500

    Computes model_c instead of just computing model_c_log

--- resample.sh
+++ resample.sh
@@ -12,7 +12,15 @@ do
 echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_right_log_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_c_right_log_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
 done >> joblist_resample
 
+for file in ./sigmoid_fit/smoothed/*_model_c_left_20mm.txt
+do
+echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_left_20mm.txt)_GM_50_surface_left.obj $file ./surf_map/$(basename $file _model_c_left_20mm.txt)_left_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+done >> joblist_resample
 
+for file in ./sigmoid_fit/smoothed/*_model_c_right_20mm.txt
+do
+echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_right_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_c_right_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+done >> joblist_resample
 
 for file in ./sigmoid_fit/smoothed/*_model_ratio_left_20mm.txt
 do
