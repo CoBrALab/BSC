d1884d82fd36540c02c67fb2607c38626731a68c
parent41 <oparent456@gmail.com>
2021-03-18 21:03:56 -0400

    Change from CIVET 1.1.12 to CIVET 2.1.0

--- resample.sh
+++ resample.sh
@@ -4,30 +4,30 @@
 
 for file in ./sigmoid_fit/smoothed/*_model_c_left_log_20mm.txt
 do
-echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_left_log_20mm.txt)_GM_50_surface_left.obj $file ./surf_map/$(basename $file _model_c_left_log_20mm.txt)_left_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+echo surface-resample /opt/quarantine/CIVET/2.1.0/build/CIVET-2.1.0/models/icbm/icbm_avg_mid_sym_mc_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_left_log_20mm.txt)_GM_50_surface_left.obj $file ./surf_map/$(basename $file _model_c_left_log_20mm.txt)_left_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
 done > joblist_resample
 
 for file in ./sigmoid_fit/smoothed/*_model_c_right_log_20mm.txt
 do
-echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_right_log_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_c_right_log_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+echo surface-resample /opt/quarantine/CIVET/2.1.0/build/CIVET-2.1.0/models/icbm/icbm_avg_mid_sym_mc_right.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_right_log_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_c_right_log_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
 done >> joblist_resample
 
 for file in ./sigmoid_fit/smoothed/*_model_c_left_20mm.txt
 do
-echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_left_20mm.txt)_GM_50_surface_left.obj $file ./surf_map/$(basename $file _model_c_left_20mm.txt)_left_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+echo surface-resample /opt/quarantine/CIVET/2.1.0/build/CIVET-2.1.0/models/icbm/icbm_avg_mid_sym_mc_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_left_20mm.txt)_GM_50_surface_left.obj $file ./surf_map/$(basename $file _model_c_left_20mm.txt)_left_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
 done >> joblist_resample
 
 for file in ./sigmoid_fit/smoothed/*_model_c_right_20mm.txt
 do
-echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_right_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_c_right_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+echo surface-resample /opt/quarantine/CIVET/2.1.0/build/CIVET-2.1.0/models/icbm/icbm_avg_mid_sym_mc_right.obj ./surfaces/GM_50_surfaces/$(basename $file _model_c_right_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_c_right_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
 done >> joblist_resample
 
 for file in ./sigmoid_fit/smoothed/*_model_ratio_left_20mm.txt
 do
-echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_ratio_left_20mm.txt)_GM_50_surface_left.obj $file ./surf_map/$(basename $file _model_ratio_left_20mm.txt)_left_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+echo surface-resample /opt/quarantine/CIVET/2.1.0/build/CIVET-2.1.0/models/icbm/icbm_avg_mid_sym_mc_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_ratio_left_20mm.txt)_GM_50_surface_left.obj $file ./surf_map/$(basename $file _model_ratio_left_20mm.txt)_left_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
 done >> joblist_resample
 
 for file in ./sigmoid_fit/smoothed/*_model_ratio_right_20mm.txt
 do
-echo surface-resample /opt/quarantine/CIVET/1.1.12/build/CIVET-1.1.12/models/surf_reg_model_left.obj ./surfaces/GM_50_surfaces/$(basename $file _model_ratio_right_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_ratio_right_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
+echo surface-resample /opt/quarantine/CIVET/2.1.0/build/CIVET-2.1.0/models/icbm/icbm_avg_mid_sym_mc_right.obj ./surfaces/GM_50_surfaces/$(basename $file _model_ratio_right_20mm.txt)_GM_50_surface_right.obj $file ./surf_map/$(basename $file _model_ratio_right_20mm.txt)_right_surfmap.sm ./sigmoid_fit/resampled/$(basename $file .txt)_rsl.txt
 done >> joblist_resample
